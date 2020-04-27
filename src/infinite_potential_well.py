cc88826da9cb32a4e457b7fddc90566295b5288d
antelk <alojic00@fesb.hr>
2020-04-27 15:32:23 +0200

    file name change

---
+++ src/infinite_potential_well.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+import numpy as np
+from scipy.integrate import quad
+
+def Ψ(x, n):
+    A = np.sqrt(2/L)
+    return A * np.sin(n * np.pi / L * x)
+
+def pdf(x, n):
+    return Ψ(x, n)**2
+
+L = 1
+quantum_numbers = [1, 2, 3]
+x = np.linspace(0, 1, 100)
+
+fig, axs = plt.subplots(3, 1, sharex='all', figsize=(6, 8))
+for i, n in enumerate(quantum_numbers):
+    axs[i].plot(x, pdf(x, n))
+    axs[i].fill_between(x, y1=pdf(x, n), color='lightsteelblue')
+
+    I, _ = quad(pdf, 0, L, args=(n))
+    axs[i].set_title(f'$n$ = {n} \t $|\Psi|^2$ = {round(I, 2)}')
+
+    axs[i].vlines(x=0, ymin=0, ymax=np.max(pdf(x, n)), color='r')
+    axs[i].vlines(x=1, ymin=0, ymax=np.max(pdf(x, n)), color='r')
+    axs[i].text(0, np.max(pdf(x,n)), '$\infty$', color='r')
+    axs[i].text(1, np.max(pdf(x,n)), '$\infty$', color='r')
+
+
+plt.setp(axs, yticks=[]) # yticks removed
+plt.xlabel('$L$ [nm]')
+plt.tight_layout()
+plt.show()  
+
