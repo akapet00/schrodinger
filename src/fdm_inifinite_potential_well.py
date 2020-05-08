aed594804775b6adead5670f5aec0b6ded44090e
antelk <alojic00@fesb.hr>
2020-05-08 16:40:43 +0200

    fdm finished; minor issues

--- src/fdm_inifinite_potential_well.py
+++ src/fdm_inifinite_potential_well.py
@@ -1,6 +1,7 @@
 import argparse
 import numpy as np
 from scipy.constants import h, hbar, m_e, pi
+from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 from matplotlib.legend_handler import HandlerTuple
 import seaborn as sns
@@ -21,7 +22,7 @@ def mape(true, predict):
 
 def rmse(true, predict):
     """root mean squared error"""
-    return np.sqrt(np.mean((true-predict)**2))
+    return np.sqrt(mean_squared_error(true, predict))
 
 def psi_close_form(x, n, L):
     """analyitcal solution for 1-D wave eqn"""
@@ -32,81 +33,68 @@ def pdf_close_form(x, n, L):
     r"""probability of locating an electron in x \in (0, L)"""
     return psi_close_form(x, n, L)**2
 
-def p(x):
-    """source function"""
+def V(x):
+    """Potential function"""
     return 0. * x
 
-def fdm(N, xmin, L, bcs, args):
-    """(A/dx**2 - B/(2*dx)) * psi(x-dx) + (C-2*A/dx**2) * psi(x) + (A/dx**2 + B/(2*dx)) * psi(x+dx)"""
+def fdm(N, xmin, L, bcs):
+    """(- hbar**2/(2*m_e) * d^2/dx^2) psi = E * psi"""
     dx = (L - xmin)/(N - 1)         # distance between grid points
     xmesh = np.linspace(xmin, L, N) # mesh
 
-    A, B, C = args                  # eqn arguments
-    psi_0, psi_L = bcs              # boundary conditions
+    psi_0, psi_L = bcs       
 
-    a = A/dx**2 - B/(2*dx)      
-    b = C-2*A/dx**2
-    c = A/dx**2  + B/(2*dx)
+    # hamiltonian formulation
+    d_xx = np.diag(np.full(N, 2), k=0) +\
+          np.diag(np.full(N-1, -1), k=-1) +\
+          np.diag(np.full(N-1, -1), k=+1)
+    H = - hbar/(2 * m_e * dx**2) * d_xx + np.diag(np.full(N, V(xmesh)), k=0)
 
-    # a * psi[i-1] + b * psi[i] + c * psi[i+1] = 0
-    # LHS @ sol = RHS + bcs 
-    LHS = np.diag(np.full(N, a), k=0) +\
-         np.diag(np.full(N-1, b), k=-1) + np.diag(np.full(N-1, b), k=+1) +\
-         np.diag(np.full(N-2, c), k=2) 
-    
-    RHS = p(xmesh).reshape(-1, 1)
-    
-    sol = np.linalg.solve(LHS, RHS)
-    sol[0, ] = psi_0
-    sol[-1, ] = psi_L
-    
-    return sol
+    # eigenvalues (E) and eigenvector (psi)
+    E, U = np.linalg.eig(H)
+ 
+    # eigenvector normalization
+    U = U * np.linalg.norm(U)
 
+    # probability density function |psi|^2
+    pdf = U * np.conj(U)
+    return E, U, pdf
+    
 def main():
     args = parser.parse_args()
     N = args.num_of_points          # number of grid points
-    xmin = .0                       # left boundary
+    xmin = 0.0                       # left boundary
     L = 1.0                         # right boundary
-    psi_0 = .0                      # left Dirichlet bc
-    psi_L = .0                      # right Dirichlet bc
+    psi_0 = 0.0                      # left Dirichlet bc
+    psi_L = 0.0                      # right Dirichlet bc
     x = np.linspace(xmin, L, N)
     _x = np.linspace(xmin, L, 1000)
 
-    principal_quantum_numbers = [1, 2, 3]       
+    principal_quantum_numbers = [1, 2, 3]
     nonreletivistic_energies = []
     for n in principal_quantum_numbers:
         E = n**2 * h**2 / (8 * m_e * L)
         nonreletivistic_energies.append(E)
 
-    fig, axs = plt.subplots(len(principal_quantum_numbers), 1, sharex='all', figsize=(8, 12))
-    for i, (n, E) in enumerate(zip(principal_quantum_numbers, nonreletivistic_energies)):
-        # hbar**2/(2*m_e) * psi_xx + E * psi = 0 
-        A = hbar**2/(2*m_e)
-        B = 0 
-        C = E
-        psi = fdm(N, xmin, L, (psi_0, psi_L), (A, B, C)).flatten()
-        pdf = psi**2
-
+    # H psi = E psi
+    # H = - hbar**2/(2*m_e) * d^2/dx^2 -> eigenvalue problem
+    E, U, pdf = fdm(N, xmin, L, bcs=[psi_0, psi_L])
+    
+    fig, axs = plt.subplots(len(principal_quantum_numbers), 1, sharex='all', figsize=(7, 9))
+    for i, (n, E_analytic) in enumerate(zip(principal_quantum_numbers, nonreletivistic_energies)):
         psi_analytic = psi_close_form(x, n, L)
         pdf_analytic = pdf_close_form(x, n, L)
     
         l1, = axs[i].plot(_x, psi_close_form(_x, n, L), 'b-')
-        l2, = axs[i].plot(_x, pdf_close_form(_x, n, L), 'r--')
-        l3, = axs[i].plot(x, psi, 'bo', markersize=7)
-        l4, = axs[i].plot(x, psi**2, 'r^', markersize=7)
+        l2, = axs[i].plot(_x, pdf_close_form(_x, n, L), 'r-')
+        l3, = axs[i].plot(x, U[:, i], 'bo')
+        l4, = axs[i].plot(x, pdf[:, i], 'ro')
 
-        axs[i].set_title(f'RMSE = {round(rmse(pdf_analytic, pdf), 3)}')
+        axs[i].set_title(f'RMSE = {np.round(rmse(pdf_analytic, pdf[:, i]), 5)}')
         axs[i].legend([l1, l2, (l3, l4)], [r'$\psi(x)$', r'$|\psi(x)|^2$', 'FDM'], 
                       handler_map={tuple: HandlerTuple(ndivide=None)})
-    
     plt.xlabel(r'$x$')
     plt.tight_layout()
-    plt.show()  
-
-
+    plt.show() 
 if __name__ == "__main__":
     main()
-
-
-
-
