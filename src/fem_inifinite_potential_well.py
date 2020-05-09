b4da3e19eb20a9f7600ca3cc170429b32ff132f6
antelk <alojic00@fesb.hr>
2020-05-09 16:02:32 +0200

    fem starters

--- src/fem_inifinite_potential_well.py
+++ src/fem_inifinite_potential_well.py
@@ -6,6 +6,10 @@ from matplotlib.legend_handler import HandlerTuple
 import seaborn as sns
 sns.set()
 
+from fdm_inifinite_potential_well import psi_close_form, pdf_close_form
+from utils.metrics import rmse
+from utils.plotting import latexconfig
+
 parser = argparse.ArgumentParser(description='Takes the number of finite elements.')
 parser.add_argument('-n', '--num_elements',
                     type=int,
@@ -14,19 +18,6 @@ parser.add_argument('-n', '--num_elements',
                         an electron in the infinite quantum well based on the given\
                         number of finite elements.',
                     default=10)
-                             
-def rmse(true, predict):
-    return np.sqrt(np.mean((true - predict)**2))
-
-def psi_close_form(x, n, L):
-    """analyitcal solution for 1-D wave eqn"""
-    amp = np.sqrt(2/L)
-    return amp * np.sin(n * pi / L * x)
-
-def pdf_close_form(x, n, L):
-    r"""probability of locating an electron in x \in (0, L)"""
-    return psi_close_form(x, n, L)**2
-
 
 def assemble_l(lhss, lhs_glob):
     if lhs_glob.shape[0]!=lhs_glob.shape[1]:
@@ -41,7 +32,7 @@ def assemble_r(rhss, rhs_glob):
         rhs_glob[i:i+rhs.shape[0], ] += rhs
     return rhs_glob
 
-def fem(N, xmin, L, bcs, args):
+def fem(N, xmin, L, bcs):
     raise NotImplementedError('Yet to appear.')
 
 def main():
@@ -60,27 +51,31 @@ def main():
         E = n**2 * h**2 / (8 * m_e * L)
         nonreletivistic_energies.append(E)
 
-    sol = []
-    fig, axs = plt.subplots(len(principal_quantum_numbers), 1, sharex='all', figsize=(8, 12))
-    for i, (n, E) in enumerate(zip(principal_quantum_numbers, nonreletivistic_energies)):
-        # hbar**2/(2*m_e) * psi_xx + E * psi = 0 
-        A = hbar**2/(2*m_e)
-        B = 0 
-        C = E
-        psi = fem(N, xmin, L, (psi_0, psi_L), (A, B, C)).flatten()
-        sol.append(psi)
+    E, psi, pdf = fem(N, xmin, L, (psi_0, psi_L))
+
+    fig, axs = plt.subplots(len(principal_quantum_numbers), 1, sharex='all', figsize=(7, 9))
+    for i, (n, E) in enumerate(zip(principal_quantum_numbers, nonreletivistic_energies)):  
+        psi_analytic = psi_close_form(x, n, L)
+        pdf_analytic = pdf_close_form(x, n, L)
     
         l1, = axs[i].plot(_x, psi_close_form(_x, n, L), 'b-')
-        l2, = axs[i].plot(_x, pdf_close_form(_x, n, L), 'r--')
-        l3, = axs[i].plot(x, psi, 'bo', markersize=7)
-        l4, = axs[i].plot(x, psi**2, 'r^', markersize=7)
+        l2, = axs[i].plot(_x, pdf_close_form(_x, n, L), 'r-')
+        l3, = axs[i].plot(x, psi[:, i], 'bo')
+        l4, = axs[i].plot(x, pdf[:, i], 'ro')
 
         axs[i].legend([l1, l2, (l3, l4)], [r'$\psi(x)$', r'$|\psi(x)|^2$', 'FDM'], 
                       handler_map={tuple: HandlerTuple(ndivide=None)})
+        axs[i].set_title(f'RMSE = {np.round(rmse(pdf_analytic, pdf[:, i]), 5)}')
+        axs[i].legend([l1, l2, (l3, l4)], [r'$\psi(x)$', r'$|\psi(x)|^2$', 'FDM'], 
+                      handler_map={tuple: HandlerTuple(ndivide=None)})
+    plt.xlabel(r'$x$')
+    plt.tight_layout()
+    plt.show() 
     
     plt.xlabel(r'$x$')
     plt.tight_layout()
     plt.show()  
 
 if __name__ == "__main__":
+    latexconfig()
     main()
